27c98362bb21d4b269470b2c8bd8c83322817349
yaya <yasminccfe@gmail.com>
2023-10-18 01:47:36 -0300

    feat: add page skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+      <h1>Conversor de Texto</h1>
+
+      <div className="container">
+        <form>
+            <textarea name="texto" placeholder="Digite sua frase..."></textarea>
+            <div className="buttons">
+                <button type="button">maiúscula</button>
+                <button type="button">minúscula</button>
+                <button type="button">especial</button>
+            </div>
+            <textarea disabled></textarea>
+        </form>
+      </div>
     </div>
   );
 }
